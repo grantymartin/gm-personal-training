3972f7ecc104ae8ad87e82a6948f342b609cd1d3
Ubuntu <ubuntu@ip-172-31-5-223.eu-west-2.compute.internal>
2020-04-29 13:32:41 +0000

    stripe payments set up

--- gm_pt/urls.py
+++ gm_pt/urls.py
@@ -6,6 +6,7 @@ from products import urls as urls_products
 from cart import urls as urls_cart
 from search import urls as urls_search
 from categories import urls as urls_categories
+from checkout import urls as urls_checkout
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -17,6 +18,7 @@ urlpatterns = [
     url(r'^classes/', include(urls_classes)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^categories/', include(urls_categories)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
